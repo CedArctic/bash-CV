dd7d062a965e967befec5a04a09084efa631d1d6
CedArctic <kdoumani98@gmail.com>
2018-09-09 15:55:44 +0300

    Visual Changes
    - Removed <pre> tags in favor of tables, lists etc.
    - Improved mobile responsiveness.
    - Switched to Lucida Console as the default font
    - Changed styling scheme (heading and paragraph usage etc.)
    - Styled some elements
    - Various other visual (mainly) fixes

--- js/main.js
+++ js/main.js
@@ -1,33 +1,87 @@
 // Functions for each action-command.
 
 function help(){
-  return "<pre>Usage:\n\
-   \nall\t\t\t\tGive me the complete picture\
-   \nabout\t\t\t\tEverything you ever wanted to know about John Smith\
-   \ncontact\t\t\t\tHow to get in touch with John Smith\
-   \nlinks\t\t\t\tSee what links John Smith wants you to click on\
-   \nskills\t\t\t\tGet to know how good John Smith is at stuff\
-   \ncv\t\t\t\tDownload John Smith's CV</pre>";
+  return "<h2><span style=\"color:#eb926d;\">Help:</span></h2><table>\
+  <tr>\
+    <td>all</td>\
+    <td>Give me the complete picture</td>\
+  </tr>\
+  <tr>\
+    <td>about</td>\
+    <td>Everything you ever wanted to know about John Smith</td>\
+  </tr>\
+  <tr>\
+    <td>contact</td>\
+    <td>How to get in touch with John Smith</td>\
+  </tr>\
+  <tr>\
+    <td>links</td>\
+    <td>See what links John Smith wants you to click on</td>\
+  </tr>\
+  <tr>\
+    <td>skills</td>\
+    <td>Get to know how good John Smith is at stuff</td>\
+  </tr>\
+  <tr>\
+    <td>cv</td>\
+    <td>Download John Smith's CV</td>\
+  </tr>\
+  <tr>\
+    <td>rickroll</td>\
+    <td>Never Gonna Give You Up</td>\
+  </tr></table>";
 }
 
 function contact(){
-  return "<pre><span style=\"color:#cc6666;\">Contact:</span>\n\
-   \n  Email:\t\t<a href=\"mailto:john.smith@emailprovider.com\">john.smith@emailprovider.com</a>\n\
-   \n  Telephone:\t\t<a href=\"tel:+45-657-56234\">+45-657-56234</a>\n\
-   \n  Address:\t\tJohn Smith\
-   \n\t\t\t205 Autumn Street\
-   \n\t\t\t5472\
-   \n\t\t\tLondon\
-   \n\t\t\tUK</pre>";
+  return "<h2><span style=\"color:#cc6666;\">Contact:</span></h2><table>\
+  <tr>\
+    <td>Email</td>\
+    <td><a href=\"mailto:john.smith@emailprovider.com\">john.smith@emailprovider.com</a></td>\
+  </tr>\
+  <tr>\
+    <td>Telephone</td>\
+    <td><a href=\"tel:+45-657-56234\">+45-657-56234</a></td>\
+  </tr>\
+  <tr>\
+    <td>Address</td>\
+    <td>John Smith</td>\
+  </tr>\
+  <tr>\
+    <td> </td>\
+    <td>205 Autumn Street</td>\
+  </tr>\
+  <tr>\
+    <td> </td>\
+    <td>5472</td>\
+  </tr>\
+  <tr>\
+    <td> </td>\
+    <td>London</td>\
+  </tr>\
+  <tr>\
+    <td> </td>\
+    <td>UK</td>\
+  </tr></table>";
 }
 
 function skills(){
-  return "<pre><span style=\"color:#81a2be;\">Skills:</span>\n\n\
-  Photoshop\t\t[============        ] 60%\n\
-  HTML\t\t\t[==========          ] 50%\n\
-  Premiere Pro\t\t[================    ] 80%\n\
-  Rickrolling\t\t[====================] 100%\n\
-  </pre>";
+  return "<h2><span style=\"color:#81a2be;\">Skills:</span></h2><table>\
+  <tr>\
+    <td>Photoshop</td>\
+    <td>▰▰▰▰▰▰▱▱▱▱ 60%</td>\
+  </tr>\
+  <tr>\
+    <td>HTML</td>\
+    <td>▰▰▰▰▰▱▱▱▱▱ 50%</td>\
+  </tr>\
+  <tr>\
+    <td>Premiere Pro</td>\
+    <td>▰▰▰▰▰▰▰▰▱▱ 80%</td>\
+  </tr>\
+  <tr>\
+    <td>Rickrolling</td>\
+    <td>▰▰▰▰▰▰▰▰▰▰ 100%</td>\
+  </tr></table>";
 }
 
 function about(){
@@ -35,97 +89,97 @@ function about(){
 }
 
 function cv(){
-  return "<pre><span style=\"color:#8abeb7;\">CV:</span>\tDownload from <a href=\"src/cv.pdf\" target=\"_blank\" style=\"text-decoration: underline;\">here</a>.</pre>";
+  return "<span style=\"color:#8abeb7;\"><h2>CV:</h2></span><p>\tDownload from <a href=\"src/cv.pdf\" target=\"_blank\" style=\"text-decoration: underline;\">here</a>.</p>";
 }
 
 function credits(){
-  return "<pre>Built by <a href=\"https://www.github.com/CedArctic\" target=\"_blank\"><i class=\"fab fa-github\"></i> CedArctic</a></pre>";
+  return "<p>Built by <a href=\"https://www.github.com/CedArctic\" target=\"_blank\"><i class=\"fab fa-github\"></i> CedArctic</a></p>";
 }
 
 function links(){
-  return "<pre><span style=\"color: #b5bd68;\">Links:</span>\n\n\
-  <a href=\"https://www.linkedin.com\" target=\"_blank\"><i class=\"fab fa-linkedin\"></i> LinkedIn</a>\n\
-  <a href=\"https://www.github.com\" target=\"_blank\"><i class=\"fab fa-github\"></i> Github</a>\n\
-  <a href=\"https://www.twitter.com\" target=\"_blank\"><i class=\"fab fa-twitter\"></i> Twitter</a>\n\
-  <a href=\"https://www.facebook.com\" target=\"_blank\"><i class=\"fab fa-facebook\"></i> Facebook</a>\n\
-  </pre>";
+  return "<span style=\"color: #b5bd68;\"><h2>Links:</h2></span><ul>\
+  <li><a href=\"https://www.linkedin.com\" target=\"_blank\"><i class=\"fab fa-linkedin\"></i> LinkedIn</a></li>\
+  <li><a href=\"https://www.github.com\" target=\"_blank\"><i class=\"fab fa-github\"></i> Github</a></li>\
+  <li><a href=\"https://www.twitter.com\" target=\"_blank\"><i class=\"fab fa-twitter\"></i> Twitter</a></li>\
+  <li><a href=\"https://www.facebook.com\" target=\"_blank\"><i class=\"fab fa-facebook\"></i> Facebook</a></li>\
+  </ul>";
 }
 
 function rickroll(){
-  return "<pre>We're no strangers to love\n\
-    You know the rules and so do I\n\
-    A full commitment's what I'm thinking of\n\
-    You wouldn't get this from any other guy\n\
-    \n\
-    I just wanna tell you how I'm feeling\n\
-    Gotta make you understand\n\
-    \n\
-    Never gonna give you up\n\
-    Never gonna let you down\n\
-    Never gonna run around and desert you\n\
-    Never gonna make you cry\n\
-    Never gonna say goodbye\n\
-    Never gonna tell a lie and hurt you\n\
-    \n\
-    We've known each other for so long\n\
-    Your heart's been aching, but\n\
-    You're too shy to say it\n\
-    Inside, we both know what's been going on\n\
-    We know the game and we're gonna play it\n\
-    \n\
-    And if you ask me how I'm feeling\n\
-    Don't tell me you're too blind to see\n\
-    \n\
-    Never gonna give you up\n\
-    Never gonna let you down\n\
-    Never gonna run around and desert you\n\
-    Never gonna make you cry\n\
-    Never gonna say goodbye\n\
-    Never gonna tell a lie and hurt you\n\
-    \n\
-    Never gonna give you up\n\
-    Never gonna let you down\n\
-    Never gonna run around and desert you\n\
-    Never gonna make you cry\n\
-    Never gonna say goodbye\n\
-    Never gonna tell a lie and hurt you\n\
-    \n\
-    (Ooh, give you up)\n\
-    (Ooh, give you up)\n\
-    Never gonna give, never gonna give\n\
-    (Give you up)\n\
-    Never gonna give, never gonna give\n\
-    (Give you up)\n\
-    \n\
-    We've known each other for so long\n\
-    Your heart's been aching, but\n\
-    You're too shy to say it\n\
-    Inside, we both know what's been going on\n\
-    We know the game and we're gonna play it\n\
-    \n\
-    I just wanna tell you how I'm feeling\n\
-    Gotta make you understand\n\
-    \n\
-    Never gonna give you up\n\
-    Never gonna let you down\n\
-    Never gonna run around and desert you\n\
-    Never gonna make you cry\n\
-    Never gonna say goodbye\n\
-    Never gonna tell a lie and hurt you\n\
-    \n\
-    Never gonna give you up\n\
-    Never gonna let you down\n\
-    Never gonna run around and desert you\n\
-    Never gonna make you cry\n\
-    Never gonna say goodbye\n\
-    Never gonna tell a lie and hurt you\n\
-    \n\
-    Never gonna give you up\n\
-    Never gonna let you down\n\
-    Never gonna run around and desert you\n\
-    Never gonna make you cry\n\
-    Never gonna say goodbye\n\
-    Never gonna tell a lie and hurt you</pre>";
+  return "<p>We're no strangers to love<br>\
+    You know the rules and so do I<br>\
+    A full commitment's what I'm thinking of<br>\
+    You wouldn't get this from any other guy<br>\
+    <br>\
+    I just wanna tell you how I'm feeling<br>\
+    Gotta make you understand<br>\
+    <br>\
+    Never gonna give you up<br>\
+    Never gonna let you down<br>\
+    Never gonna run around and desert you<br>\
+    Never gonna make you cry<br>\
+    Never gonna say goodbye<br>\
+    Never gonna tell a lie and hurt you<br>\
+    <br>\
+    We've known each other for so long<br>\
+    Your heart's been aching, but<br>\
+    You're too shy to say it<br>\
+    Inside, we both know what's been going on<br>\
+    We know the game and we're gonna play it<br>\
+    <br>\
+    And if you ask me how I'm feeling<br>\
+    Don't tell me you're too blind to see<br>\
+    <br>\
+    Never gonna give you up<br>\
+    Never gonna let you down<br>\
+    Never gonna run around and desert you<br>\
+    Never gonna make you cry<br>\
+    Never gonna say goodbye<br>\
+    Never gonna tell a lie and hurt you<br>\
+    <br>\
+    Never gonna give you up<br>\
+    Never gonna let you down<br>\
+    Never gonna run around and desert you<br>\
+    Never gonna make you cry<br>\
+    Never gonna say goodbye<br>\
+    Never gonna tell a lie and hurt you<br>\
+    <br>\
+    (Ooh, give you up)<br>\
+    (Ooh, give you up)<br>\
+    Never gonna give, never gonna give<br>\
+    (Give you up)<br>\
+    Never gonna give, never gonna give<br>\
+    (Give you up)<br>\
+    <br>\
+    We've known each other for so long<br>\
+    Your heart's been aching, but<br>\
+    You're too shy to say it<br>\
+    Inside, we both know what's been going on<br>\
+    We know the game and we're gonna play it<br>\
+    <br>\
+    I just wanna tell you how I'm feeling<br>\
+    Gotta make you understand<br>\
+    <br>\
+    Never gonna give you up<br>\
+    Never gonna let you down<br>\
+    Never gonna run around and desert you<br>\
+    Never gonna make you cry<br>\
+    Never gonna say goodbye<br>\
+    Never gonna tell a lie and hurt you<br>\
+    <br>\
+    Never gonna give you up<br>\
+    Never gonna let you down<br>\
+    Never gonna run around and desert you<br>\
+    Never gonna make you cry<br>\
+    Never gonna say goodbye<br>\
+    Never gonna tell a lie and hurt you<br>\
+    <br>\
+    Never gonna give you up<br>\
+    Never gonna let you down<br>\
+    Never gonna run around and desert you<br>\
+    Never gonna make you cry<br>\
+    Never gonna say goodbye<br>\
+    Never gonna tell a lie and hurt you</p>";
 }
 
 // Main Function
